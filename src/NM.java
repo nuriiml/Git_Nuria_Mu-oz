6b2bd868a485e3812a4589d5466337f694387804
Nuria Muñoz <nuriatoboso@gmail.com>
2023-02-06 21:24:58 +0100

    Modificación 2

--- src/NM.java
+++ src/NM.java
@@ -4,6 +4,7 @@ public class NM {
 	public static void main(String[] args) {
 		System.out.println("Alumno: Nuria Muñoz");
 		System.out.println("Modificación desde Master");
+		System.out.println("Modificación desde Develop");
 
 	}
 
